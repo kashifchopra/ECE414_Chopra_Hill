75f963b4956dda6eca8e306417c6729ded37256e
Benjamin Hill <hillba@ad.lafayette.edu>
2024-09-04 14:36:46 -0400

    Lab 2 start

---
+++ Lab2/led_out.c
@@ -0,0 +1,15 @@
+#include "led_out.h"
+#include "pico/stdlib.h"
+#include <cstdint>
+
+void led_out_init(){
+
+    //low level code to initialise port
+}
+
+void led_out_write(uint8_t val){
+    
+    //low level code to initialise the port
+
+
+}
