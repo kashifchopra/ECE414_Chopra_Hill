2e976daa05cc42de0e80f37efc198542d2b05eb1
Kashif Chopra <choprak@ad.lafayette.edu>
2024-10-18 14:57:48 -0400

    started working on FSM

---
+++ Lab5/tckFnct_calculator.c
@@ -0,0 +1,18 @@
+#include "TFTMaster.h"
+#include "pico/stdlib.h"
+
+//first define states - 
+//initial, num1, op, num2, result, error, (maybe div0 too)
+
+
+uint32_t tckFnct_Calculator(uint32_t input){
+
+//define variables/flags 
+
+//define state change scenarios with switch case
+
+
+//define actions within each state with switch case 
+
+
+}
---
+++ Lab5/tckFnct_calculator.h
@@ -0,0 +1,12 @@
+#ifndef TCKFNCT_CALCULATOR
+#define TCKFNCT_CALCULATOR
+
+#include "TFTMaster.h"
+#include "pico/stdlib.h"
+#include "stdint.h"
+
+//FSM for all calculator operations
+uint32_t tckFnct_Calculator(uint32_t input);
+
+
+#endif
