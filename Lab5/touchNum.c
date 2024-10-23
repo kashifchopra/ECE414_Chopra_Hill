f589e020081ff63d44fe60773d289ef35cae0f38
Benjamin Hill <ben@Benjamins-MacBook-Pro-2.local>
2024-10-23 13:35:59 -0400

    rec

--- Lab5/touchNum.c
+++ Lab5/touchNum.c
@@ -1,7 +1,29 @@
 //module to return an integer value to FSM for button press based on position of the touchscreen touched. 
+#include "ts_lcd.h"
+#include "tckFnct_calculator.h"
+#include "hardware/gpio.h"
+#include "TFTMaster.h"
+#include <stdio.h>
+#include "pico/stdlib.h"
 
-int touchRead(int x, int y){
+int input(){
+     struct TSPoint p;
+                p.x = 0;
+                p.y = 0;
+                p.z = 0;
+    
+    uint32_t savedX;
+    uint32_t savedY;
 
+    if(get_ts_lcd(&p.z)){ //sense press 
+        savedY = interpolateY(p.x); //get x and y touch coordinates 
+        savedX = interpolateX(p.y);
+    }
+
+    return getInput(savedX, savedY); //plug into getInput, return specific button number
+}
+
+int getInput(int x, int y){
     //FIRST ROW 
     if((x > 10 && x < 75) && (y > 40 && y < 80)){
         return 7;
@@ -30,9 +52,33 @@ int touchRead(int x, int y){
         return 10;
     }
 
+    //THIRD ROW 
+    if((x > 10 && x < 75) && (y > 140 && y < 180)){
+        return 7;
+    }
+    if((x > 85 && x < 150) && (y > 140 && y < 180)){
+        return 8;
+    }
+    if((x > 160 && x < 225) && (y > 140 && y < 180)){
+        return 9;
+    }
+    if((x > 245 && x < 310) && (y > 140 && y < 180)){
+        return 10;
+    }
 
-
-
+    //FOURTH ROW    
+    if((x > 10 && x < 75) && (y > 190 && y < 230)){
+        return 7;
+    }
+    if((x > 85 && x < 150) && (y > 190 && y < 230)){
+        return 8;
+    }
+    if((x > 160 && x < 225) && (y > 190 && y < 230)){
+        return 9;
+    }
+    if((x > 245 && x < 310) && (y > 190 && y < 230)){
+        return 10;
+    }
 
 
 }
