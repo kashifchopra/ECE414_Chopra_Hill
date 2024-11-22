205dbc3734007ef1be9b9f2aef02d204b95f6ad4
Benjamin Hill <ben@Benjamins-MacBook-Pro-2.local>
2024-11-22 13:49:01 -0500

    PIR

---
+++ StudySense/PIR.c
@@ -0,0 +1,50 @@
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
+#include <stdio.h>
+#include <stdbool.h>
+
+
+//NEED TIMER MODULE FOR INTERNAL COUNTER
+//   - while (WHILE LOOP) in occupied, every time internal counter clocks a minute,
+//     run check
+
+
+//           while(1){
+// 
+//               if(motion){
+//                  occ_tmr_cnt = 0;
+//               }
+//               if(!motion && occ_tmr_cnt < 20){
+//                  occ_tmr_cnt++;
+//                  internal_cnt = 0;
+//               } else if (!motion && occ_tmr_cnt >= 20){
+//                  STATE = UNOCCUPIED;
+//               }
+//              
+//          
+//           }
+
+
+bool motion; //PIR data, high or low
+bool occupied; //boolean flag
+uint32_t internal_cnt; //counter incremented once every second in OCCUPIED state
+uint32_t occ_tmr_cnt; //cycles of no motion
+uint32_t timer_limit = 20; //SUBJECT TO CHANGE FOR TESTING PURPOSES
+
+
+
+
+void pir_init(){
+   adc_init();
+
+
+}
+
+
+//function to get pir data, HIGH will flag motion var to true, vice versa
+
+
+
+
+
