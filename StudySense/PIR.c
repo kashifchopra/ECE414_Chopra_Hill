61812d57f88f07af400acdcde422b19c6b8d83a2
Benjamin Hill <ben@Benjamins-MacBook-Pro-2.local>
2024-11-22 14:28:54 -0500

    PIR WORK

--- StudySense/PIR.c
+++ StudySense/PIR.c
@@ -1,3 +1,4 @@
+#include "PIR.h"
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/adc.h"
@@ -31,12 +32,37 @@ bool occupied; //boolean flag
 uint32_t internal_cnt; //counter incremented once every second in OCCUPIED state
 uint32_t occ_tmr_cnt; //cycles of no motion
 uint32_t timer_limit = 20; //SUBJECT TO CHANGE FOR TESTING PURPOSES
+const uint32_t pir_gpio = 16;
 
 
 
-
-void pir_init(){
+void pir_init(){ //low level code to initialize port
    adc_init();
+   gpio_set_dir(pir_gpio, GPIO_IN);
+}
+
+bool pir_read(){ //boolean output of pir
+    if(gpio_get(pir_gpio)){
+        motion = true;
+    } else {
+        motion = false;
+    }
+    return motion;
+}
+
+static enum STATE {
+    INIT,
+    UNOCC,
+    OCC
+}
+
+void tickFnct_pir(){
+
+
+
+
+
+
 
 
 }
